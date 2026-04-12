dcecbe3bb52f129c03d98d88855082f836d90cc7
Albert <molikodev@gmail.com>
2026-04-12 19:02:23 -0300

    yep.. moving project stuff.

--- tools/rnnoise_retrainer/export.sh
+++
@@ -1,3 +0,0 @@
-wiredeck-rnnoise export-gpu-model \
-  ~/.config/wiredeck/models/supervised-52.bin \
-  --checkpoint /home/moliko/projects/wiredeck_2.0/tools/rnnoise_retrainer/artifacts/gpu-supervised-run/checkpoints/wiredeck_gpu_epoch_052.pt
